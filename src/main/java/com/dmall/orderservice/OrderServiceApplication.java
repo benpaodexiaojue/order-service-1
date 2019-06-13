151973997b208a8d34c690ac20c209862dcd1826
garen.chen@oocl.com <garen.chen@oocl.com>
2019-06-13 15:01:39 +0800

    register service

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -3,10 +3,12 @@ package com.dmall.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableSwagger2
 //TODO: enable feign client
+@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
